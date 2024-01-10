5e6db77d95c6bf29498fe9e131911dc19f4a01f7
Peetah <peterleemartin@gmail.com>
2024-01-10 00:01:18 -0600

    added functions to pull weather and forecast based on coordinates. commented in some notes for to-do for tomorrow

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,3 @@
-/* https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}}&appid={API key}&units=imperial
-
-user presses button
-
-write a function to capture the location provided by the user
-Pull that data from the weathermap API for current weather
-Get the lat and lon for the city
-use that for the five day forecast
-
-write a function that creates cards for current weather and 5 day forecast
-
-
-
-*/
 const requestURL = ""
 const cityInput = document.querySelector("#citySearch");
 const submitBtn = document.querySelector("#submit");
@@ -22,7 +8,7 @@ function citySearch(event) {
     const userSearch = cityInput.value.trim();
     console.log(userSearch);
     // outputs the city name as userSearch
-    // then run function to show the weather
+    // then run function to get coordinates
     getCoords(userSearch);
 }
 
@@ -32,28 +18,69 @@ function getCoords(city) {
     return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=304e6560d21d5b309a457696f7869351`)
     .then(function(response) {
         return response.json();
-    
     })
     .then(function(data) {
+        const closestMatch = data[0];
+        const latCoords = closestMatch.lat;
+        const lonCoords = closestMatch.lon;
         console.log(data);
-        showWeather();
+        console.log(latCoords, lonCoords);
+        showWeather(latCoords, lonCoords);
+        showForecast(latCoords, lonCoords);
+
+        // also run a showForecast function which will do the same
+        // thing as showWeather, but for 
     })
 }
 
-function showWeather(city) {
-    //pull weather data from weathermap
-}
 
-// get city coordinates from database, use those to plug into 
-function showForecast(coordinates) {
-    return fetch()
+// Function to pull the current weather of searched location
+// API call and pull relevant data (Current time, Temp, Wind speed, Humidity)
+function showWeather(lat, lon) {
+    //pull weather data from openweather
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=304e6560d21d5b309a457696f7869351&units=imperial`)
     .then(function(response) {
         return response.json();
     })
-    .then(function(data) {
-        console.log(data);
+    .then(function(weather) {
+        // START HERE
+        //create variables for each datatype needed
+        // const weatherVal = {time, temp, wind, humidity}
+        // const 
+
+        //create html styling for each variable
+        // this will be in "#current" div
+
+        console.log(weather);
+        
+        
     })
 }
+
+// Function to pull 5 day forecast of searched location
+// API call and pull relevant data (Date, Temp, Wind speed, Humidity)
+function showForecast(lat, lon) {
+    //pull forecast data from openweather
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=304e6560d21d5b309a457696f7869351&units=imperial`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(weather) {
+        //create variables for each datatype needed
+        //
+        // const weatherVal = 
+        console.log(weather);
+        //run a for loop for a 5 day period
+        //create html styling for each day 
+        // will be in the "#forecast" div
+        
+
+    })
+}
+
+//create a function to save the data into local storage
+//that will show up in a div called "recent searches"
+//clicking on one of those links will take you to the weather of that city
 
 
 
